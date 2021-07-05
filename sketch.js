8459c89e7c4518e90f9663fea9af9b70eb1dd7e1
narutoops <85290543+narutoops@users.noreply.github.com>
2021-07-05 16:16:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var appleImg,apple;
+var orangeL,oleafimg
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple .png");
+  oleafimg = loadImage("orangeLeaf.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  rabbit.x = World.mouseX;
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+ 
+ var select_sprites =  Math.round(random(1,3));
+  if (frameCount % 80 == 0) 
+  {
+     if (select_sprites == 1) { 
+       createApples(); 
+ }
+ else if(select_sprites ==2){
+   createOrange();
+ }
+}
+  drawSprites();
+}
+
+
+
+function createApples() {
+  apple = createSprite(random(50, 350),40, 10, 10);
+  apple.addImage(appleImg);
+  apple.scale=0.07;
+  apple.velocityY = 3;
+  apple.lifetime = 150;
+    
+  }
+  function createOrange() { 
+    orangeL = createSprite(random(50, 350),40, 10, 10);
+     orangeL.addImage(oleafimg);
+      orangeL.scale=0.08;
+       orangeL.velocityY = 3;
+        orangeL.lifetime = 150; }
+
+
+
+
+
